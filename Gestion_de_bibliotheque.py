c8ea8a4022765334963bb6ce5837bdb3aa35f8bc
mutsungajames <gloiremusumba1212@gmail.com>
2024-05-25 18:20:19 +0100

    Branche surchage

--- Gestion_de_bibliotheque.py
+++ Gestion_de_bibliotheque.py
@@ -19,7 +19,17 @@ class Livre(ItemBibliotheque):
     def __init__(self, titre, auteur, isbn, editeur):
         super().__init__(titre, auteur, isbn)  # Appel du constructeur de la classe parent
         self.editeur = editeur  # Editeur spécifique au Livre
+# Classe DVD qui hérite de ItemBibliotheque et surcharge la méthode emprunter
+class DVD(ItemBibliotheque):
+    def __init__(self, titre, auteur, isbn, duree):
+        super().__init__(titre, auteur, isbn)  # Appel du constructeur de la classe parent
+        self.duree = duree  # Durée spécifique au DVD
 
+    # Surcharge de la méthode emprunter pour inclure une vérification d'âge
+    def emprunter(self, age):
+        if age < 18:
+            return "n'est pas disponible"  # Les utilisateurs de moins de 18 ans ne peuvent pas emprunter de DVD
+        return super().emprunter()  # Appel de la méthode emprunter du parent
 
 
 

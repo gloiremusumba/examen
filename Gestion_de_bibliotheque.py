aa3397556255a5dcf502012ec3af4e21059ad7f3
mutsungajames <gloiremusumba1212@gmail.com>
2024-05-25 18:17:52 +0100

    Branche heritage

--- Gestion_de_bibliotheque.py
+++ Gestion_de_bibliotheque.py
@@ -14,6 +14,11 @@ class ItemBibliotheque():
 
     def retourner(self):
         self.disponible = "disponible"  # L'item est retourné et disponible
+# Classe Livre qui hérite de ItemBibliotheque
+class Livre(ItemBibliotheque):
+    def __init__(self, titre, auteur, isbn, editeur):
+        super().__init__(titre, auteur, isbn)  # Appel du constructeur de la classe parent
+        self.editeur = editeur  # Editeur spécifique au Livre
 
 
 

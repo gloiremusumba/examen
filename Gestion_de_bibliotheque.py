2bb4c9f6faba313e556f7a8781e84faab27c7973
mutsungajames <gloiremusumba1212@gmail.com>
2024-05-25 18:41:30 +0100

    Branche encapsulation

--- Gestion_de_bibliotheque.py
+++ Gestion_de_bibliotheque.py
@@ -41,6 +41,15 @@ class DVD(ItemBibliotheque):
     def verifier_disponibilite(item):
         return item.disponible  # Retourne la disponibilité de l'item, qu'il soit un Livre ou un DVD
 
+# Création d'instances de Livre et DVD
+livre1 = Livre("1984", "George", "1289", "Éditions fridy/uniluk")
+dvd1 = DVD("Inception", "Christopher Nolan", "987654321", 148)
+
+# Exemple d'emprunt et de retour
+livre1.emprunter()
+dvd1.emprunter(25)
+print(verifier_disponibilite(livre1))  # Affiche False car le livre a été emprunté
+print(verifier_disponibilite(dvd1))
 
 
 
